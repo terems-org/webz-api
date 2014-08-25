0a403b0cf3fd435a4824b9b29cadf2cfdf4973fe
Oleksandr Tereschenko <Oleksandr@terems-pad.(none)>
2014-08-25 14:09:01 +0300

    introduced the concept of file inflation

---
+++ src/main/java/org/terems/webz/cache/ChildPathNamesHolder.java
@@ -0,0 +1,14 @@
+package org.terems.webz.cache;
+
+import java.util.Collection;
+
+/** TODO !!! describe !!! **/
+public class ChildPathNamesHolder {
+
+	/** TODO !!! describe !!! **/
+	public Collection<String> childPathNames;
+
+	/** TODO !!! describe !!! **/
+	public Object folderHash;
+
+}
